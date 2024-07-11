ae7a99acb60e6494504272bdb2a449b38eca4c61
KU-Shu <boldest_waltz_0v@icloud.com>
2024-07-11 15:59:36 +0900

    2407111600

--- hello.py
+++ hello.py
@@ -4,12 +4,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("hp.html")
+    return render_template("home.html")
 
 @app.route('/news')
 def news():
     return render_template("news.html")
-
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.debug = True

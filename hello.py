950ff94ddcf8f98a764c1a68d5451d0d6e8da096
KU-Shu <boldest_waltz_0v@icloud.com>
2024-07-11 14:58:41 +0900

    test

---
+++ hello.py
@@ -0,0 +1,16 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/news')
+def news():
+    return render_template("news.html")
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
